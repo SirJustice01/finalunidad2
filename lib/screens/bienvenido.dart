d60f554a541da6c1bfe3b0b32271f52366993d24
unknown <sandraariascajincho@gmail.com>
2022-12-27 00:10:09 -0500

    test 3

--- lib/screens/bienvenido.dart
+++ lib/screens/bienvenido.dart
@@ -16,79 +16,81 @@ class BienvenidoScreen extends StatelessWidget {
       body: Container(
         decoration: const BoxDecoration(color: Color(0xffffffff)),
         child: Center(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 10,
-              ),
-              Image.asset('assets/avatar.jpg'),
-              const SizedBox(
-                height: 10,
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.supervised_user_circle,
-                    size: 30,
-                    color: Colors.deepPurple,
-                  ),
-                  title: Text('Javier Enrique Choquetico Condori'),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 10,
                 ),
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.alternate_email_rounded,
-                    size: 30,
-                    color: Colors.deepPurple,
-                  ),
-                  title: Text(
-                    'javier@gmail.com',
+                Image.asset('assets/avatar.jpg'),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.supervised_user_circle,
+                      size: 30,
+                      color: Colors.deepPurple,
+                    ),
+                    title: Text('Javier Enrique Choquetico Condori'),
                   ),
                 ),
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.privacy_tip_outlined,
-                    size: 30,
-                    color: Colors.deepPurple,
-                  ),
-                  title: Text(
-                    'Datos y Privacidad',
-                  ),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.lock_clock_rounded,
-                    size: 30,
-                    color: Colors.deepPurple,
-                  ),
-                  title: Text(
-                    'Seguridad',
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.alternate_email_rounded,
+                      size: 30,
+                      color: Colors.deepPurple,
+                    ),
+                    title: Text(
+                      'javier@gmail.com',
+                    ),
                   ),
                 ),
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.wallet_giftcard,
-                    size: 30,
-                    color: Colors.deepPurple,
-                  ),
-                  title: Text(
-                    'Pagos y Suscripciones',
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.privacy_tip_outlined,
+                      size: 30,
+                      color: Colors.deepPurple,
+                    ),
+                    title: Text(
+                      'Datos y Privacidad',
+                    ),
                   ),
                 ),
-              ),
-            ],
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.lock_clock_rounded,
+                      size: 30,
+                      color: Colors.deepPurple,
+                    ),
+                    title: Text(
+                      'Seguridad',
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.wallet_giftcard,
+                      size: 30,
+                      color: Colors.deepPurple,
+                    ),
+                    title: Text(
+                      'Pagos y Suscripciones',
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
